212b06a04d21df56daf69fd45924f762d7aa9051
MilesXdoom <134221369+MilesXdoom@users.noreply.github.com>
2023-05-27 19:14:59 -0500

    Create js.js

---
+++ js.js
@@ -0,0 +1,21 @@
+JS
+
+<script>
+
+ const serviceBtns = document.querySelectorAll('.service-btn');
+
+ serviceBtns.forEach(btn => {
+
+ btn.addEventListener('click', event => {
+
+ const service = event.target.dataset.service;
+
+ document.querySelectorAll('.service').forEach(el => el.classList.remove('active'));
+
+ document.getElementById(service).classList.add('active');
+
+ });
+
+ });
+
+ </script>
