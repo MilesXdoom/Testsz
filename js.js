e7eed11c67c39e47cd34ef148d4e7c4cdc5b4a3a
MilesXdoom <134221369+MilesXdoom@users.noreply.github.com>
2023-05-27 23:28:21 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,8 @@
-<script>
- const serviceBtns = document.querySelectorAll('.service-btn');
- serviceBtns.forEach(btn => {
+const serviceBtns = document.querySelectorAll('.service-btn');
+serviceBtns.forEach(btn => {
  btn.addEventListener('click', event => {
  const service = event.target.dataset.service;
  document.querySelectorAll('.service').forEach(el => el.classList.remove('active'));
  document.getElementById(service).classList.add('active');
  });
- });
- </script>
+});
