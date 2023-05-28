e0bcdee26388a16f4a02cce540be88351e230f65
MilesXdoom <134221369+MilesXdoom@users.noreply.github.com>
2023-05-27 20:11:50 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+<script>
+ const serviceBtns = document.querySelectorAll('.service-btn');
+ serviceBtns.forEach(btn => {
+ btn.addEventListener('click', event => {
+ const service = event.target.dataset.service;
+ document.querySelectorAll('.service').forEach(el => el.classList.remove('active'));
+ document.getElementById(service).classList.add('active');
+ });
+ });
+ </script>
