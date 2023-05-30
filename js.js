854b021a143b9a904fcf56ed39fee49001ae0cb9
MilesXdoom <134221369+MilesXdoom@users.noreply.github.com>
2023-05-30 01:15:45 -0500

    Delete js.js

--- js.js
+++
@@ -1,8 +0,0 @@
-const serviceBtns = document.querySelectorAll('.service-btn');
-serviceBtns.forEach(btn => {
- btn.addEventListener('click', event => {
- const service = event.target.dataset.service;
- document.querySelectorAll('.service').forEach(el => el.classList.remove('active'));
- document.getElementById(service).classList.add('active');
- });
-});
